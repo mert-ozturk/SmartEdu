1e186dc1086fff4e04b0d743d90ff32d83bf3e68
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-07-02 13:15:30 +0300

    test2

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const pageRoute = require ('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
+const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db',{
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+
+}).then(()=>{
+    console.log('DB connected')
+})
+
+//Template Engine
+app.set('view engine','ejs');
+
+//Middlewares
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+//Routes
+app.use('/',pageRoute);
+app.use('/courses',courseRoute);
+
+const port = 3000;;
+app.listen(port,()=>{
+    console.log(`App login ${port}`)
+})
